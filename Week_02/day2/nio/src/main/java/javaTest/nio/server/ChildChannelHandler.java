f66f12369b290c90db713f793e8982c365c0d06c
cloud <cloud1888888@163.com>
2021-01-21 23:47:14 +0800

    week2-work

---
+++ Week_02/day2/nio/src/main/java/javaTest/nio/server/ChildChannelHandler.java
@@ -0,0 +1,11 @@
+package javaTest.nio.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+public class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        socketChannel.pipeline().addLast(new DiscardServerHandler());
+    }
+}
