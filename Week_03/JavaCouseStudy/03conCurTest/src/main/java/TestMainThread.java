e5dc4b041a603fb6db64265834a29b53817a0398
cloud <cloud1888888@163.com>
2021-01-31 00:53:54 +0800

    week3-work

--- Week_03/JavaCouseStudy/03conCurTest/src/main/java/TestMainThread.java
+++
@@ -1,7 +0,0 @@
-public class TestMainThread {
-
-    public static void main(String[] args){
-
-      Thread.currentThread().getThreadGroup().getParent().list();
-    }
-}
--- Week_03/README.md
+++ Week_03/README.md
@@ -1,1 +1,3 @@
-学习笔记
+### 简要说明
+##### 1. v1 小版本是上一次作业httpclient和netty server的简单组合。
+##### 2. v2 小版本实现了过滤器，访问地址：http://localhost:8804/server01
